c2177e48d2573fc10be5e512972b2ccdf79680dc
dieasquo <dieasquo@gmail.com>
2021-01-28 10:59:44 +0700

    install php-ml with composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'efb52a1ad0288a88a46a64da41fbcdd160db0c28',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'efb52a1ad0288a88a46a64da41fbcdd160db0c28',
+    ),
+    'php-ai/php-ml' => 
+    array (
+      'pretty_version' => '0.9.0',
+      'version' => '0.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fd8b70629e16ed67307fe150ba25738449b9a6b2',
+    ),
+  ),
+);
