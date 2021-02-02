3d2d7877ca4e59ddac2409866aa7604b752d9b95
dieasquo <dieasquo@gmail.com>
2021-02-02 18:13:40 +0700

    fix style Header Foward Chaining page

--- foward.php
+++ foward.php
@@ -8,7 +8,8 @@
      <div class="carousel-item active">
        <div class="carousel-container">
          <p class="animate__animated fanimate__adeInUp">
-         <div class="foward" style="background-color: white; margin: 5rem; padding: 1rem 5rem; border-radius: 10px">
+         <h2 class="mt-2">Foward Chaining</h2>
+         <div class="foward" style="background-color: white; margin: 3rem; padding: 1rem 5rem; border-radius: 10px">
 
            <?php
             include "connect_foward.php";
@@ -44,9 +45,9 @@
             }
 
             if ($s['ifyes'] == "0" && $s['ifno'] == "0") {
-              echo "<br/><span style=' font-size:0.9em; color: #000;'><h4>Hasil Konsultasi :</h4><br> " ;
+              echo "<br/><span style=' font-size:0.9em; color: #000;'><h4>Hasil Konsultasi :</h4><br> ";
               echo "<h3><b>" . $penyakit . "</b></h3><br>";
-              echo "</ul><a class='btn-primary btn' href='foward.php'>Coba Konsultasi Lagi</a></span>"; 
+              echo "</ul><a class='btn-primary btn' href='foward.php'>Coba Konsultasi Lagi</a></span>";
             }
             ?>
 
@@ -57,21 +58,25 @@
  </section><!-- End Hero -->
  <center>
 
-   <h2 class="pt-4">Rule yang Di lewati :</h2>
    <section id="about" class="contact">
      <div class="container">
        <?php
-        if ($s['ifyes'] == "0" && $s['ifno'] == "0") {
-          $rule = mysql_query("SELECT * FROM rule_temporary 
-            left join gejala on rule_temporary.pilihan=gejala.id 
-              where username='alopakar' AND pilihan NOT LIKE 'M%'");
-          while ($o = mysql_fetch_array($rule)) {
-            if ($o['jawaban'] == 'Y') {
-              $jawaban = "<span style='color:green'>Yes</span>";
-            } else {
-              $jawaban = "<span style='color:red'>No</span>";
+        if (!isset($penyakit)) {
+          echo "<h2 class='pt-4'>Rule yang Di lewati :</h2>";
+          if ($s['ifyes'] == "0" && $s['ifno'] == "0") {
+            $rule = mysql_query("SELECT * FROM rule_temporary 
+              left join gejala on rule_temporary.pilihan=gejala.id 
+                where username='alopakar' AND pilihan NOT LIKE 'M%'");
+            while ($o = mysql_fetch_array($rule)) {
+              if ($o['jawaban'] == 'Y') {
+                $jawaban = "<span style='color:green'>Yes</span>";
+              } else {
+                $jawaban = "<span style='color:red'>No</span>";
+              }
+              echo "<span> $o[pertanyaan] $jawaban</span><br>";
             }
-            echo "<span> $o[pertanyaan] $jawaban</span><br>";
+          } else {
+            echo "";
           }
         } else {
           echo "";
