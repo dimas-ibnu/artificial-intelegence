b40502512db58137e0906a39e79c156346b6ff35
dieasquo <dieasquo@gmail.com>
2021-02-05 03:33:47 +0700

    update db_backward

--- DATABASE/db_backward.sql
+++ DATABASE/db_backward.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5deb2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Jan 24, 2021 at 01:05 AM
--- Server version: 8.0.22-0ubuntu0.20.04.3
--- PHP Version: 7.4.3
+-- Host: localhost
+-- Generation Time: Feb 04, 2021 at 09:33 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,146 +18,142 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_coba`
+-- Database: `db_backward`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_gejala`
+-- Table structure for table `base_knowledge`
 --
 
-CREATE TABLE `tb_gejala` (
-  `id` int NOT NULL,
-  `G001` tinyint(1) DEFAULT NULL,
-  `G002` tinyint(1) DEFAULT NULL,
-  `G003` tinyint(1) DEFAULT NULL,
-  `G004` tinyint(1) DEFAULT NULL,
-  `G005` tinyint(1) DEFAULT NULL,
-  `G006` tinyint(1) DEFAULT NULL,
-  `G007` tinyint(1) DEFAULT NULL,
-  `G008` tinyint(1) DEFAULT NULL,
-  `G009` tinyint(1) DEFAULT NULL,
-  `G010` tinyint(1) DEFAULT NULL,
-  `G011` tinyint(1) DEFAULT NULL,
-  `G012` tinyint(1) DEFAULT NULL,
-  `G013` tinyint(1) DEFAULT NULL,
-  `G014` tinyint(1) DEFAULT NULL,
-  `G015` tinyint(1) DEFAULT NULL,
-  `G016` tinyint(1) DEFAULT NULL,
-  `G017` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `base_knowledge` (
+  `id` int(11) NOT NULL,
+  `kode_penyakit` varchar(100) NOT NULL,
+  `daftar_gejala` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tb_gejala`
+-- Dumping data for table `base_knowledge`
 --
 
-INSERT INTO `tb_gejala` (`id`, `G001`, `G002`, `G003`, `G004`, `G005`, `G006`, `G007`, `G008`, `G009`, `G010`, `G011`, `G012`, `G013`, `G014`, `G015`, `G016`, `G017`) VALUES
-(1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(2, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(3, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0),
-(4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1),
-(5, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0),
-(6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0),
-(7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0);
+INSERT INTO `base_knowledge` (`id`, `kode_penyakit`, `daftar_gejala`) VALUES
+(1, 'P1', 'G01 G02 G03 G04 G05 G06 G07'),
+(2, 'P2', 'G01 G06 G08 G09 G10'),
+(3, 'P3', 'G15 G16 G17'),
+(4, 'P4', 'G01 G18 G19 G20');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_gejala_1`
+-- Table structure for table `gejala`
 --
 
-CREATE TABLE `tb_gejala_1` (
-  `id` int NOT NULL,
+CREATE TABLE `gejala` (
   `kode` varchar(5) NOT NULL,
-  `gejala` varchar(200) NOT NULL,
-  `id_penyakit` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `nama_gejala` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tb_gejala_1`
+-- Dumping data for table `gejala`
 --
 
-INSERT INTO `tb_gejala_1` (`id`, `kode`, `gejala`, `id_penyakit`) VALUES
-(1, 'G001', 'Mata menutup', 1),
-(2, 'G002', 'Jengger kebiruan', 1),
-(3, 'G003', 'Nafsu makan berkurang\r\n', 1),
-(4, 'G004', 'Kelemahan kaki\r\n', 1),
-(5, 'G005', 'Nafsu Makan Berkurang', 2),
-(6, 'G006', 'Mata Menutup', 2),
-(7, 'G007', 'Tumuh Lambat', 2),
-(8, 'G008', 'Lumpuh\r\n', 2),
-(9, 'G009', 'Tumbuh Lambat\r\n', 3),
-(10, 'G010', 'Diare\r\n', 3),
-(11, 'G011', 'Bulu Berdiri', 3),
-(12, 'G012', 'Becak Putih Pada Jengger\r\n', 3),
-(13, 'G013', 'Bercak Putih berjengger', 4),
-(14, 'G014', 'Diare\r\n', 4),
-(15, 'G015', 'anemia', 7),
-(16, 'G016', 'produksi telur turun\r\n', 7),
-(17, 'G017', 'Stress', 7),
-(18, 'G018', 'Diare', 5),
-(19, 'G019', 'Tumbuh Lambat', 5),
-(20, 'G020', 'Bulu berdiri', 6),
-(21, 'G021', 'Depresi', 6);
+INSERT INTO `gejala` (`kode`, `nama_gejala`) VALUES
+('G01', 'Sakit kepala'),
+('G02', 'Pusing'),
+('G03', 'Migran'),
+('G04', 'Pendarahan dari hidung'),
+('G05', 'Wajah kemerahan'),
+('G06', 'Mudah Lelah'),
+('G07', 'Penglihatan kabur'),
+('G08', 'Mual berlebihan'),
+('G09', 'Berkeringat'),
+('G10', 'Cemas'),
+('G11', 'Tegang'),
+('G12', 'Nyeri pada bagian tubuh'),
+('G13', 'Denyut jantung tidak teratur'),
+('G14', 'Pembengkakan pada kaki dan perut'),
+('G15', 'Cepat haus'),
+('G16', 'Sering kencing'),
+('G17', 'Penurunan berat badan'),
+('G18', 'Berat badan berlebihan'),
+('G19', 'Mengalami aterosklerosis seacara spontan'),
+('G20', 'Kurang aktivitas'),
+('G21', 'Mudah mengantuk'),
+('G22', 'Kaki bengkak'),
+('G23', 'Rasa sakit atau pegal pada tengkuk kepala'),
+('G24', 'Pegal sampai ke pundak'),
+('G25', 'Selulit'),
+('G26', 'Sakit pada lutut'),
+('G27', 'Varices'),
+('G28', 'Sulit bernafas'),
+('G29', 'Sendi Terasa nyeri'),
+('G30', 'Sendi meradang'),
+('G31', 'Sendi bengkak'),
+('G32', 'Sendi panas'),
+('G33', 'Sendi kaku'),
+('G34', 'Kaki keseleo'),
+('G35', 'Benjolan disekitar sendi yang meradang'),
+('G36', 'Tidak keluar urin'),
+('G37', 'Nafsu makan menurun'),
+('G38', 'merasa anemia'),
+('G39', 'Proporsi lemak bertambah'),
+('G40', 'Sakit pada lambung'),
+('G41', 'Sakit pada usus');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_penyakit`
+-- Table structure for table `penyakit`
 --
 
-CREATE TABLE `tb_penyakit` (
-  `id` int NOT NULL,
-  `penyakit` varchar(100) NOT NULL,
-  `info` varchar(1000) NOT NULL,
-  `solusi` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `penyakit` (
+  `kode` varchar(5) NOT NULL,
+  `nama_penyakit` varchar(200) NOT NULL,
+  `keterangan` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tb_penyakit`
+-- Dumping data for table `penyakit`
 --
 
-INSERT INTO `tb_penyakit` (`id`, `penyakit`, `info`, `solusi`) VALUES
-(1, 'Pullorum', 'Pullorum disebabkan oleh bakteri Salmonella pullorum, yaitu suatu bakteri bersifat gram negatif, tidak bergerak, berbentuk batang, fakultatif aerob dan tidak berspora, dan mampu bertahan di tanah hingga satu tahun. Penyakit Pullorum merupakan penyakit menular pada ayam yang menimbulkan kerugian ekonomi yang besar, menyebabkan kematian yang sangat tinggi terutama pada anak ayam umur 1-10 hari.', 'Pencegahan diutamakan pada sanitasi dan tata laksana serta manajemen pemeliharaan yang baik. Pengobatan pullorum dapat dilakukan dengan penyuntikan antibiotik seperti cocillin, neo terramycin ke dada ayam.'),
-(2, 'Aspergillosis atau Brooder Pneumonia', 'Aspergillosis atau Brooder Pneumonia adalah suatu penyakit yang disebabkan oleh jamur atau cendawan dari genus Aspergillus, yang paling patogen adalah Aspergillus fumigatus, Aspergillus flavus dan Aspergillus niger. Penyakit ini menyerang manusia dan hewan. Pada unggas terutama menyerang alat pernapasan.', 'Belum ada vaksin yang efektif untuk pencegahan. Hewan penderita sebaiknya diisolasi. Pakan ternak dijaga jangan sampai terkontaminasi cendawan. Belum ada obat yang efektif dan ekonomis untuk aspergillosis pada unggas.'),
-(3, 'Candidiasis', 'Candidiasis merupakan penyakit mikal yang disebabkan oleh Candida. Candida telah dikenal dan dipelajari sejak abad ke-18. Penyakit ini terutama disebabkan oleh hygiene yang tidak baik. Candida dapat hidup sebagai saprobe tanpa menyebabkan kelainan pada berbagai permukaan tubuh manusia dan hewan. Spesies Candida yang dikenal banyak menimbulkan penyakit baik pada manusia maupun hewan adalah Candida albicans.', 'Pencegahan candidiasis hanya bisa dilakukan dengan meningkat kan standar sanitasi, menghindari pemberian obat, antibiotik, dan coccidiostat, serta menghindari stimulan pertumbuhan berlebihan yang dapat mempengaruhi flora normal pada saluran pencernaan. Pengobatan dapat dilakukan menggunakan cooper sulfat dengan takaran 1 : 2000 (1 bagian cooper sulfat dan 2000 bagian air minum).'),
-(4, 'Ringworm', 'Ringworm adalah penyakit menular pada permukaan kulit yang disebabkah oleh cendawan. Cendawan penyebab penyakit ini termasuk dalam kelompok Dermatophyta. Terdapat 4 (empat) genus yaitu Trichophyton, Microsporum, Epidermophyton, dan Keratinomyces, yang menyebabkan penyakit badan hewan adalah Trichophyton dan Microsporum.', 'Pencegahan ringworm dilakukan dengan menjaga kebersihan kulit dan kesehatan tubuh hewan. Ringworm jenis tertentu dapat sembuh dengan sendirinya. Pengobatan dapat dilakukan dengan 2 (dua)  cara, yaitu dengan olesan atau pengobatan per oral melalui mulut. Obat yang digunakan mengandung lemak, jodium sulfa atau asam salisilat.'),
-(5, 'Ascariasis', 'Ascariasis adalah penyakit cacing yang menyerang unggas dan disebabkan oleh Ascaridia galli. Sinonim spesies ini adalah A.lineata, A.perspicillum. Cacing ini terdapat di usus  dan duodenum hewan unggas. Pada ternak ayam sering menyerang baik tipe pedaging maupun tipe petelur, sedangkan pada ayam buras  kemungkinan tertular lebih besar karena sistem pemeliharaan yang bebas berkeliaran.', 'Pencegahan dapat dilakukan dengen menerapkan manajemen pemeliharaan yang baik dan benar. Pengobatan terhadap Ascaridia galli yang paling sering dilakukan dengan pemberian piperazine. Anthelmentik ini sangat efektif, dapat diberikan melalui makanan atau minuman.'),
-(6, 'Coccidiosis', 'Coccidiosis merupakan penyakit parasiter pada sistem pencernaan unggas akibat infeksi protozoa genus Emeria. Penyakit ini tersebar di seluruh dunia dan menyebabkan kerugian ekonomi yang besar. Coccidiosis menyebabkan pertumbuhan unggas yang tidak optimal akibat menurunnya efisiensi penyerapan nutrisi pakan. Pada kejadian yang kronis, penyakit ini dapat menyebabkan kematian yang cukup tinggi pada unggas.', 'Pencegahan Coccidiosis pada unggas dapat dilakukan dengan penerapan tindakan biosecurity dan pemberian vaksin secara teratur. Pengobatan Coccidiosis dapat dilakukan dengan pemberian obat-obatan yang bersifat coccidiostat atau coccidiocidal. Pemberian coccidiostat tidak mengeliminasi seluruh parasit dari dalam tubuh tetapi hanya menekan jumlah parasit yang ada di dalam tubuh.'),
-(7, 'Gurem (Ornithonyssus bursa)', 'Gurem (Ornithonyssus bursa) termasuk sub ordo Mesostigmata, sub kelas Ascari dan kelas Arachnida. Hama ini sangat kecil dan sulit diberantas. Gurem menghisap darah, hidup bergerombol, dan keluar pada malam hari. Gurem betina menghisap darah ayam sebanyak 0.077 mg atau jumlah yang dihisap adalah 1.8 kali berat tubuh gurem.', 'Pencegahan dapat dilakukan dengan menerapkan manajemen pemeliharaan yang baik dan menjaga kebersihan kandang dan sekitarnya. Ayam yang terserang gurem dapat diobati dengan cara memandikannya dengan campuran air sabun dan belerang.');
+INSERT INTO `penyakit` (`kode`, `nama_penyakit`, `keterangan`) VALUES
+('P1', 'Hipertensi', 'Hipertensi primer\r\n\r\nHipertensi primer menyerang 90% penderita hipertensi. Penyebabnya tidak diketahui dengan pasti dan cenderung terjadi bertahap selama bertahun-tahun. Faktor gaya hidup dan genetik diduga memiliki peranan penting.\r\n\r\nHipertensi sekunder\r\n\r\nHipertensi yang diketahui penyebabnya, terjadi pada 5-10% penderita hipertensi. Biasanya muncul tiba-tiba dan menyebabkan tekanan darah yang lebih tinggi daripada hipertensi primer.'),
+('P2', 'Jantung', '<span>Penyakit jantung adalah kondisi ketika jantung mengalami gangguan. Beberapa jenis penyakit jantung, antara lain:</span>\r\n<ul>\r\n<li>Penyakit jantung koroner, merupakan suatu penyakit jantung yang terjadi akibat penyempitan pembuluh darah di jantung.</li>\r\n<li>Penyakit jantung bawaan, merupakan suatu masalah jantung yang ditemukan sejak bayi, yang paling umum ditemukan adalah kebocoran katup jantung.</li>\r\n<li>Infeksi jantung (endokarditis), merupakan suatu infeksi pada lapisan dalam jantung.</li>\r\n<li>Gagal jantung, merupakan suatu kegagalan otot jantung untuk memompakan darah secara memadai ke seluruh tubuh.</li>\r\n</ul>'),
+('P3', 'Diabetes Militus', 'Diabetes mellitus merupakan penyakit kronis yang disebabkan oleh gagalnya organ pankreas memproduksi jumlah hormon insulin secara memadai sehingga menyebabkan peningkatan kadar glukosa dalam darah. DM merupakan salah satu penyakit tidak menular dan merupakan salah satu masalah kesehatan masyarakat yang penting.'),
+('P4', 'Hiperklosterolemia', 'Hiperkolesterolemia adalah kondisi berbahaya yang ditandai dengan tingginya kadar kolesterol dalam darah. Bila tidak ditangani, kolesterol dapat menumpuk serta mempersempit pembuluh darah. Akibatnya, penderita berisiko terserang penyakit jantung koroner.');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tb_gejala`
+-- Indexes for table `base_knowledge`
 --
-ALTER TABLE `tb_gejala`
+ALTER TABLE `base_knowledge`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tb_gejala_1`
+-- Indexes for table `gejala`
 --
-ALTER TABLE `tb_gejala_1`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `gejala`
+  ADD PRIMARY KEY (`kode`);
+
+--
+-- Indexes for table `penyakit`
+--
+ALTER TABLE `penyakit`
+  ADD PRIMARY KEY (`kode`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `tb_gejala`
+-- AUTO_INCREMENT for table `base_knowledge`
 --
-ALTER TABLE `tb_gejala`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `tb_gejala_1`
---
-ALTER TABLE `tb_gejala_1`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+ALTER TABLE `base_knowledge`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
